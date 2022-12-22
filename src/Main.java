9f943c1ed79756c16a9ce89ea8ad4f5a96a61900
AlexanderElkin1969 <eaa@mfcmmr.ru>
2022-12-22 21:15:52 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
+        task1_3();
+        task4();
+        task5();
     }
-    public static void task1(){
-    System.out.println("Задача 1");
+    public static void task1_3(){
+        System.out.println("Задача 1");
         var dog=8.0;
         var cat=3.6;
         var paper=763789;
@@ -19,7 +19,7 @@ public class Main {
         paper=paper-7639;
         System.out.println(dog+", "+cat+", "+paper);
     }
-    public static void task2(){
+    public static void task4(){
         System.out.println("Задача 2");
         var friend=19;
         System.out.println(friend);
@@ -28,7 +28,7 @@ public class Main {
         friend=friend/7;
         System.out.println(friend);
     }
-    public static void task3(){
+    public static void task5(){
         System.out.println("Задача 3");
         var frog=3.5;
         System.out.println(frog);
